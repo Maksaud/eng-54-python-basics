ff91334566137629de06017f7bd156d8bd1b0759
Maksaud <maksaud@hotmail.com>
2020-03-03 14:39:17 +0000

    exercise 3 done

---
+++ exercise_103.py
@@ -0,0 +1,11 @@
+name = "Lana"
+last_name = "Lana"
+eye_color = "Lana"
+hair_color = "Lana"
+
+name = input("What new name would you like? ")
+last_name = input("What new last name would you like? ")
+eye_color = input("What new eye colour would you like? ")
+hair_color = input("What new hair colour would you like? ")
+
+print(f'Hello {name} {last_name}! Welcome, your eyes are {eye_color} and your hair color is {hair_color}.')
