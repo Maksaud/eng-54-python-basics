74a483a21ae4993e1144b50f4c375406a4045db3
Maksaud <maksaud@hotmail.com>
2020-03-03 15:49:16 +0000

    improved exercise 6

--- exercise_106.py
+++ exercise_106.py
@@ -3,21 +3,23 @@
 # equate something
 
 # As a user, I want to be able to guess a number and know if i got it correct or not, so that I can know if I won or not.
-
-
 # We should define/assign number to a variable called magic_number
 magic_number = 5
-
-# I need to ask user for input
-user_number = int(input('What is your chosen number between 0 and 10 '))
-
-
-# I need to check if this input matches a magic_number
-if magic_number == user_number:
-    print('You are correct')
-else:
-    print("You were wrong")
+count = 6
+while(count != 0):
+    # I need to ask user for input
+    user_number = int(input('What is your chosen number between 0 and 10 '))
 
 
-# I need to let the user know if the response was write or not
-#self five
+    # I need to check if this input matches a magic_number
+    if magic_number == user_number:
+        print('You are correct')
+        break
+    else:
+        print(f"You were wrong, You have {count} turns left")
+        count = count-1
+        continue
+
+
+    # I need to let the user know if the response was write or not
+    #self five
