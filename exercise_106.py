bf1d65bf443c54fa7c6e1966c1981b4363f9487b
Maksaud <maksaud@hotmail.com>
2020-03-04 09:36:11 +0000

    update exercise 106

--- exercise_106.py
+++ exercise_106.py
@@ -4,9 +4,14 @@
 
 # As a user, I want to be able to guess a number and know if i got it correct or not, so that I can know if I won or not.
 # We should define/assign number to a variable called magic_number
+
+
 magic_number = 5
+
+
 count = 6
-while count != 0:
+
+while count >= 0:
     # I need to ask user for input
     user_number = int(input('What is your chosen number between 0 and 10 '))
 
@@ -17,7 +22,7 @@ while count != 0:
         break
     else:
         print(f"You were wrong, You have {count} turns left")
-        count = count-1
+        count -= 1
         continue
 
 
